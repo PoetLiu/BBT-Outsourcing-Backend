c38b15ce1e55e666e17e0c92537e172646f7a0ba
ye59080125 <zyr_liye@foxmail.com>
2018-12-29 15:45:57 +0800

    增加跨域配置和更改接口为9090

---
+++ main/src/main/java/com/bangdna/main/config/cors/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.bangdna.main.config.cors;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @program: bangbangtuan-outsourcing-back-end
+ * @author: Abel Lee
+ * @create: 2018-12-29 15:44
+ **/
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
