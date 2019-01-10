beae02862a4b593865ff57638df5915ceeafe71d
Albert Liu <457566268@qq.com>
2019-01-10 19:23:34 +0800

    Disable WebSecurity csrf to avoid PUT request 403 error.

--- main/src/main/java/com/bangdna/main/config/security/BBTSercurityConfig.java
+++ main/src/main/java/com/bangdna/main/config/security/BBTSercurityConfig.java
@@ -12,13 +12,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity
 public class BBTSercurityConfig extends WebSecurityConfigurerAdapter {
-
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests()
-                .anyRequest()
-                .permitAll();
+        http
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .csrf().disable();
     }
 }
