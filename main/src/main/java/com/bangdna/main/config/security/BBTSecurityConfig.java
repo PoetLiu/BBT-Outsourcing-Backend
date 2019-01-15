72effc70350bdbbdad129a35f9cff4fc92e8b308
Albert Liu <457566268@qq.com>
2019-01-15 16:50:59 +0800

    Use basic auth for test.

---
+++ main/src/main/java/com/bangdna/main/config/security/BBTSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.bangdna.main.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.savedrequest.NullRequestCache;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * @program: bangbangtuan-outsourcing-back-end
+ * @author: Abel Lee
+ * @create: 2019-01-03 13:09
+ **/
+
+@Configuration
+@EnableWebSecurity
+public class BBTSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .requestCache()
+                .requestCache(new NullRequestCache())
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
